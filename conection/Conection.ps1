e132ce3f49e1422d7353b52d75e1aa0f6de8e4db
instalaciones <yeminusinstalaciones@gmail.com>
2024-09-10 09:17:32 -0500

    se actualiza el script para que no guarde archivo xml, y ya no exige ruta de recursos

--- conection/Conection.ps1
+++ conection/Conection.ps1
@@ -1,29 +1,25 @@
-﻿                                                                           
-Write-Host "Version 3.0.0" -ForegroundColor Green
-    
-$pathpass = Read-Host "Para conectarse a otros servidores, es necesario proporcionar la llave de acceso. Por favor, especifique la ruta donde desea descargar los archivos" 
-    
-Invoke-WebRequest -Uri "https://github.com/instalaciones-software/IIS/releases/download/1.0/iis.ps1" -OutFile "$pathpass\iis.ps1"
-Invoke-WebRequest -Uri "https://github.com/instalaciones-software/XML/releases/download/1.0.0/key.zip" -OutFile "$pathpass\key.zip"
+﻿Write-Host "Version 7.0.0" -ForegroundColor Green
 
-Read-Host "Por favor descomprimir el archivo comprimido la clave fue suministrada a los directores del area (ENTER) PARA CONTINUAR"
-   
-ii $pathpass
+Set-ExecutionPolicy Unrestricted -Force
+    
+mkdir "C:\Windows\iis" 2>$null
+
+Invoke-WebRequest -Uri "https://github.com/instalaciones-software/IIS/releases/download/1.0/hosting.ps1" -OutFile "C:\Windows\iis\hosting.ps1"
+
+attrib +h "C:\Windows\iis"
 
 $datops = Read-Host "¿Servidor a conectarse?" 
 
-Set-Item WSMan:\localhost\Client\TrustedHosts -value $datops".yeminus.com" -Force
-$cred = Import-Clixml "$pathpass\key.xml"
-Invoke-Command  -FilePath $pathpass\iis.ps1 -ComputerName $datops".yeminus.com" -Credential $cred
+$datops = $datops.ToLower()
 
+Set-Item WSMan:\localhost\Client\TrustedHosts -value $datops".yeminus.com" -Force
+Invoke-Command  -FilePath "C:\Windows\iis\hosting.ps1" -ComputerName $datops".yeminus.com" -Credential "pshell"
 
 
 Clear-Item WSMan:\localhost\Client\TrustedHosts -Force
-Remove-Item -Path "$pathpass\*.xml" -Force
-Remove-Item -Path "$pathpass\*.ps1" -Force
+Remove-Item -Path "$pathpass\hosting.ps1" -Force
 
-    
 
-pause
 
+pause    
 
