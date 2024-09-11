7d322500d112a42fa198b1f923a8215e5cb4d395
diego peña <diego251644@gmail.com>
2024-09-11 11:49:52 -0500

    se agrega funcionalidades de descargar el archivo ejecutable a descargas al usuario donde ejecutemos, tambien se agrega que el servicio winrm inicie

--- Conection/Conection.ps1
+++ Conection/Conection.ps1
@@ -1,14 +1,22 @@
-﻿Write-Host "Version 8.0.0" -ForegroundColor Green
+﻿
+Write-Host "Version 9.0.0.0" -ForegroundColor Green
 
-Set-ExecutionPolicy Unrestricted -Force
-    
+Set-ExecutionPolicy Unrestricted -Force 
+
 mkdir "C:\Windows\iis" 2>$null
+
+net start winrm
 
 Invoke-WebRequest -Uri "https://github.com/instalaciones-software/Scripts-Pshell/releases/download/1.0.0/remote.ps1" -OutFile "C:\Windows\iis\remote.ps1"
 
+Invoke-WebRequest -Uri "https://github.com/instalaciones-software/Scripts-Pshell/releases/download/1.0.0/Conections.exe" -OutFile "C:\Users\$env:USERNAME\Downloads\Conectionns.exe"
+
+Write-Host "En la carpeta Descargas del usuario $env:USERNAME se ha descargado recientemente el ejecutable. Lo más conveniente sería ejecutar la aplicación desde allí, ya que podría haber cambios en la aplicación" -ForegroundColor Green
+
+
 attrib +h "C:\Windows\iis"
 
-$datops = Read-Host "¿Servidor a conectarse?" 
+$datops = Read-Host "¿Servidor a conectarse, solo ingresa el subdominio?" 
 
 $datops = $datops.ToLower()
 
@@ -22,6 +30,7 @@ Remove-Item -Path "C:\Windows\iis\remote.ps1" -Force
 
 
 pause    
+
 
 
 
