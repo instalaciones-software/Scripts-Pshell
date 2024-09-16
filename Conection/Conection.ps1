1e6c99d76a9c4b194d480cda88fb60f00e5906e8
instalaciones <yeminusinstalaciones@gmail.com>
2024-09-16 14:33:58 -0500

    se quita el error que se presentaba al momento de descargar el archivo exe

--- Conection/Conection.ps1
+++ Conection/Conection.ps1
@@ -1,15 +1,11 @@
 ﻿
-Write-Host "Version 9.0.0.0" -ForegroundColor Green
+Write-Host "Version 1.0.10.0" -ForegroundColor Green
 
 Set-ExecutionPolicy Unrestricted -Force 
 
 mkdir "C:\Windows\iis" 2>$null
 
 Invoke-WebRequest -Uri "https://github.com/instalaciones-software/Scripts-Pshell/releases/download/1.0.0/remote.ps1" -OutFile "C:\Windows\iis\remote.ps1"
-
-Invoke-WebRequest -Uri "https://github.com/instalaciones-software/Scripts-Pshell/releases/download/1.0.0/Conections.exe" -OutFile "C:\Users\$env:USERNAME\Downloads\Conectionns.exe"
-
-Write-Host "En la carpeta Descargas del usuario $env:USERNAME se ha descargado recientemente el ejecutable. Lo más conveniente sería ejecutar la aplicación desde allí la proxima ejecuccion, ya que podría haber cambios en la aplicación" -ForegroundColor Green
 
 attrib +h "C:\Windows\iis"
 
@@ -23,7 +19,6 @@ Invoke-Command  -FilePath "C:\Windows\iis\remote.ps1" -ComputerName $datops".yem
 
 Clear-Item WSMan:\localhost\Client\TrustedHosts -Force
 Remove-Item -Path "C:\Windows\iis\remote.ps1" -Force
-
 
 
 pause    
