2b85e94b7a3a80881ead0097ac7381bcc57c00b1
yeminus <yeminusinstalaciones@gmail.com>
2024-10-30 08:00:52 -0500

    se agreegan archivos sh para la instalacion de pwsh en linux

--- bashLinux/InstallpwshLINUX.sh
+++ bashLinux/InstallpwshLINUX.sh
@@ -1,33 +1,34 @@
-###################################
-# Prerequisites
-
-# Update the list of packages
- 
-sudo apt-get update
+#!/bin/bash
 
 # Install pre-requisite packages.
+
 sudo apt-get install -y wget apt-transport-https software-properties-common
 
-# Get the version of Ubuntu
-source /etc/os-release
 
 # Download the Microsoft repository keys
-wget -q https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+
 
 # Register the Microsoft repository keys
 sudo dpkg -i packages-microsoft-prod.deb
 
- 
-# Delete the Mic
- rosoft repository keys file
-rm packages-microsoft-prod.deb
 
 # Update the list of packages after we added packages.microsoft.com
 sudo apt-get update
 
-###################################
+
 # Install PowerShell
 sudo apt-get install -y powershell
 
-# Start PowerShell
-pwsh
+#complemnetos powershell
+sudo apt-get install -y powershell gss-ntlmssp
+
+#Install PWSMan
+pwsh -Command 'Install-Module -Name PSWSMan'
+
+#Install Wsman
+pwsh -Command 'Install-WSMan'
+
+echo "Instalacion finalizada"
+
+read
--- bashLinux/connections.sh
+++ bashLinux/connections.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
 # Dowload shell ps1 for upgrade
-wget -P /home/$USER/Documentos/script/ https://github.com/instalaciones-software/Scripts-Pshell/releases/download/1.0.0/remote.ps1
+wget -P /home/updateiis/script https://github.com/instalaciones-software/Scripts-Pshell/releases/download/1.0.0/remote.ps1
+
+clear
+
+echo "
+
+********************************************************************************************************************|
+                                                                                                                    |
+                                        CONEXIONES A SERVIDORES HOSTING                                             | 
+                                                                                                                    |
+*********************************************************************************************************************
+"
 
 
 read -p "Ingresa el subdominio para conectarte al servidor:" server
 
 
-echo -e "\e[1;33mEstableciendo conexion con el servidor $server.yeminus.com\e[0m"
-
-sleep 2s
-
+echo "Estableciendo conexion con el servidor $server.yeminus.com"
 
 
 #Start PowerShell
-pwsh -Command Invoke-Command -FilePath /home/$USER/Documentos/script/remote.ps1 -ComputerName $server.yeminus.com -Authentication Negotiate -Credential pshell
+pwsh -Command Invoke-Command -FilePath /home/updateiis/script/remote.ps1 -ComputerName $server.yeminus.com -Authentication Negotiate -Credential pshell
 
 
-rm -r /home/$USER/Documentos/script/*.ps1
+rm -r /home/updateiis/script/*.ps1
