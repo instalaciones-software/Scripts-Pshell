1b01170338065c0868920d598e2777d43535d113
Diego peña <instalaciones@yeminus.com>
2025-08-26 16:52:28 -0500

    se agrega un nuevo script

---
+++ ChangePass.ps1
@@ -0,0 +1,128 @@
+﻿function Generate-RandomString {
+    param (
+        [int]$length = 12
+    )
+    
+    $chars = "QWERTYUIOPLKJHGFDASAZXCVBNMabcdefghjklmnbvcxz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~123456789"
+    
+    $randomString = -join ((1..$length) | ForEach-Object { $chars[(Get-Random -Minimum 0 -Maximum $chars.Length)] })
+    
+    return $randomString
+}
+
+$usernames = @(                                                         # Usuarios RDP 
+     @{ Name = "epineda"; ZipPassword = "Pepito" }                      # Inge
+     @{ Name = "administrator"; ZipPassword = "+-NewPw2024*#" }         # Danny Infraestructura
+     @{ Name = "instalacion"; ZipPassword = "Ocsxxi%123%" }             # Diego Infraestructura
+     @{ Name = "instalacion2"; ZipPassword = "Colombia2021**##" }       # Esteban Infraestructura
+     @{ Name = "soporte-01"; ZipPassword = "Yeminus" }                  # Jarvy   Subgerente      
+     @{ Name = "soporte-02"; ZipPassword = "15963Sopo#" }               # Harold    Director Mesa
+     @{ Name = "soporte-03"; ZipPassword = "Lc1088022547" }             # Laura mesa
+     @{ Name = "soporte-04"; ZipPassword = "Bardack085" }               # Jhon G mesa
+     @{ Name = "soporte-05"; ZipPassword = "saar98." }                  # Stiven mesa
+     @{ Name = "soporte-06"; ZipPassword = "Alana0803*" }               # Angelica mesa
+     @{ Name = "soporte-07"; ZipPassword = "Sopyem10*"}                 # Julian mesa
+     @{ Name = "consultor-01"; ZipPassword = "Valen9306." }             # Valentina mesa
+     @{ Name = "consultor-02"; ZipPassword = "Jp1088353472#" }          # Juli implentacion
+     @{ Name = "consultor-03"; ZipPassword = 'Con-Of$gem' }             # Olguita implentacion
+     @{ Name = "consultor-04"; ZipPassword = "S3bas#" }                 # Sebastian implentacion
+     @{ Name = "consultor-05"; ZipPassword = "123456Dt$" }              # Daniel taborda implentacion
+     @{ Name = "consultor-06"; ZipPassword = "lbeltran22" }             # luz stella implentacion
+     @{ Name = "consultor-07"; ZipPassword = "De1349*" }                # Diego implentacion
+     @{ Name = "consultor-08"; ZipPassword = "1053814926*p" }          # paula implentacion
+    )
+
+
+mkdir E:\Apps\geminus\datos 2>$null
+
+$folderPath = "E:\Apps\geminus\"
+
+if (-not (Test-Path -Path $folderPath)) {
+    New-Item -Path $folderPath -ItemType Directory
+}
+
+foreach ($user in $usernames) {
+    $username = $user.Name
+    $zipPassword = $user.ZipPassword
+
+    
+    $length = 30
+    
+    $randomString = Generate-RandomString -length $length
+    
+    $userFilePath = "$folderPath\datos\$env:COMPUTERNAME-$username.txt"
+    
+    Set-Content -Path $userFilePath -Value "por favor no compartir acceso, el reporte se envia cada 30 dias $username $randomString"
+    
+    $password = ConvertTo-SecureString -AsPlainText -Force -String $randomString
+    
+    Set-LocalUser -Name $username -Password $password
+    Write-Host "Cambio de clave al usuario $username"
+    
+    $zipFilePath = "$folderPath\datos\$env:COMPUTERNAME-$username.zip"
+
+    Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "a", "-tzip", "`"$zipFilePath`"", "`"$userFilePath`"", "-p$zipPassword" -NoNewWindow -Wait
+   
+    Remove-Item -Path "$folderPath\datos\*.txt" -Force
+        
+}
+
+$routefile = "E:\Apps\geminus\datos"
+$routezip = "E:\Apps\geminus\datos\datos.zip"
+
+Start-Process -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList "a", "-tzip", "`"$routezip`"", "`"$routefile`"" -NoNewWindow -Wait 
+
+
+$EmailEmisor = "noresponder@yeminus.com"
+$Asunto = "Reporte de Actividad - SRV-" + $env:COMPUTERNAME
+$CuerpoEnHTML = "<p>Cordial saludo Compañeros, Comparto el reporte</p>"
+
+$SMTPServidor = "mail.yeminus.com"
+$CodificacionCaracteres = [System.Text.Encoding]::UTF8
+
+
+$ArchivoAdjunto = "$routezip"
+
+
+$CCO = "instalaciones@yeminus.com" ,"instalaciones2@yeminus.com","epineda@yeminus.com","subgerente@yeminus.com","directorsoporte@yeminus.com","yeminusinstalaciones@gmail.com","coorinstalaciones.yeminus@gmail.com,aarias@yeminus.com,soporte2@yeminus.com,soporte1@yeminus.com,soporte10@yeminus.com,vquintero@yeminus.com,jpineda@yeminus.com,oflorez@yeminus.com,dtaborda@yeminus.com,lbeltran@yeminus.com,despinal@yeminus.com,tics@yeminus.com,ptorres@yeminus.com"
+#$CCO = "dcortes@yeminus.com" #pruebas
+
+try {
+    
+    $SMTPMensaje = New-Object System.Net.Mail.MailMessage
+    $SMTPMensaje.From = $EmailEmisor
+    $SMTPMensaje.Subject = $Asunto
+    $SMTPMensaje.Body = $CuerpoEnHTML
+    $SMTPMensaje.IsBodyHtml = $true
+    $SMTPMensaje.BodyEncoding = $CodificacionCaracteres
+    $SMTPMensaje.SubjectEncoding = $CodificacionCaracteres
+    
+    
+    $Adjunto = New-Object System.Net.Mail.Attachment($ArchivoAdjunto)
+    $SMTPMensaje.Attachments.Add($Adjunto)
+    
+    
+    foreach ($cco in $CCO) {
+        $SMTPMensaje.Bcc.Add($cco)
+    }
+        
+    
+    $SMTPCliente = New-Object Net.Mail.SmtpClient($SMTPServidor, 587)
+    $SMTPCliente.EnableSsl = $true
+    $SMTPCliente.Credentials = New-Object System.Net.NetworkCredential($EmailEmisor, "12345Aa$@/*")
+        
+    
+    $SMTPCliente.Send($SMTPMensaje)
+    Write-Output "Correo electrónico enviado correctamente."
+        
+    
+    $Adjunto.Dispose()
+}
+catch {
+    Write-Error -Message "Error al enviar correo electrónico: $_"
+}
+            
+            
+Remove-Item -Path "$folderPath\datos\*.*" -Force
+
+ 
